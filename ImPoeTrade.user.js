1832368e4aa621737f074b77d01372964865ea66
Sefriol <georgepatton1945@hotmail.com>
2016-01-06 18:17:12 +0200

    Add UserScript variables

--- ImPoeTrade.user.js
+++ ImPoeTrade.user.js
@@ -1,14 +1,17 @@
 // ==UserScript==
-// @name         Improved poe.trade
+// @name         ImPoeTrade
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @version      0.2
+// @description  Script provides small improvements to poe.trade community website
+// @author       Sefriol
 // @match        http://tampermonkey.net/index.php?ext=dhdg
+// @match        http://poe.trade/*
 // @grant        none
+// @updateURL       https://github.com/Sefriol/ImpPoETrade/master/ImPoeTrade.user.js
+// @downloadURL     https://github.com/Sefriol/ImpPoETrade/master/ImPoeTrade.user.js
+// @homepageURL     https://github.com/Sefriol/ImpPoETrade/
 // ==/UserScript==
 /* jshint -W097 */
 'use strict';
 $("[id^=item-container]").find('[class^=first-cell]').append("<a class='button secondary expand' onclick='$(this).parent().parent().parent().hide();' style='margin:0;'>Remove</a>")
 $("[id^=mid-table").hide()
-// Your code here...
