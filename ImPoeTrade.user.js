64ac7003409cdabee061453e13cd58e512202514
Sefriol <georgepatton1945@hotmail.com>
2016-01-07 04:45:56 +0200

    New feature: Add items to localstorage.

--- ImPoeTrade.user.js
+++ ImPoeTrade.user.js
@@ -16,6 +16,8 @@
 'use strict';
 var showOffline = true;
 var offline = [];
+var storedItems;
+var showStored = false;
 function itemHider(item) {
   $(item).parentsUntil($("[id^=search-results]"), ".item").hide();
 }
@@ -61,22 +63,98 @@ function toggleOffline(){
   }
 }
 
+function toggleStored() {
+  if (!showStored) {
+    $("[id=search-results-stored]").show();
+    showStored = true;
+    $("[id=search-results-stored-items]").empty();
+    if(typeof(Storage) !== "undefined") {
+      storedItems = localStorage.getItem("storedTradeItems");
+      if(storedItems !== null) {
+        storedItems = JSON.parse(storedItems);
+        $.each(storedItems, function(o) {
+          $("[id=search-results-stored-items]").append(unescape(storedItems[o]))
+          $("[id=search-results-stored-items]").find('[id=button-remove]').remove()
+          $("[id=search-results-stored-items]").find('[id=button-add-storage]').remove()
+        });
+        $("[id=search-results-stored-items]").find('[class^=table-stats]').append("<a id='button-remove-storage'class='button secondary expand' onclick='removeItem(this);' style='margin:0; width:50%'>Remove from Storage</a>");
+      } else {
+        $("[id=search-results-stored-items]").prepend("<h2><a>You have nothing stored</a></h2>");
+      }
+    } else {
+      console.log('No support for storage');
+    }
+  } else {
+    $("[id=search-results-stored]").hide();
+    showStored = false;
+  }
+}
+
+function addItem(item) {
+  if(typeof(Storage) !== "undefined") {
+    if (typeof(storedItems)=='undefined' || storedItems==null) {
+      storedItems = [];
+    }
+    storedItems.push(escape($(item).parentsUntil($("[id^=search-results]"), ".item").clone().html()));
+    localStorage.setItem("storedTradeItems", JSON.stringify(storedItems));
+    if (showStored) {
+      toggleStored();
+    }
+  } else {
+    $("[id=search-results-stored-items]").empty()
+    $("[id=search-results-stored]").show()
+    showStored = true;
+    $("[id=search-results-stored-items]").prepend("<h2><a>Your Browser doesn't support storage</a></h2>");
+  }
+}
+
+function removeItem(item) {
+  if(typeof(Storage) !== "undefined") {
+    if (typeof(storedItems)=='undefined' || storedItems==null) {
+      storedItems = [];
+      return;
+    }
+    console.log($(item).index(), $(item).parentsUntil($("[id^=search-results]"), ".item").index())
+    storedItems.splice($(item).parentsUntil($("[id^=search-results]"), ".item").index())
+    localStorage.setItem("storedTradeItems", JSON.stringify(storedItems));
+    if (showStored) {
+      showStored = false;
+      toggleStored();
+    }
+  } else {
+    $("[id=search-results-stored-items]").empty()
+    $("[id=search-results-stored]").show()
+    showStored = true;
+    $("[id=search-results-stored-items]").prepend("<h2><a>Your Browser doesn't support storage</a></h2>");
+  }
+}
+
 $( document ).ajaxComplete(function() {
-  $("[id^=item-container]").find('[class^=first-cell]').append("<a class='button secondary expand' onclick='itemHider(this);' style='margin:0;'>Remove</a>");
+  $("[id^=item-container]").find('[class^=first-cell]').append("<a id='button-remove' class='button secondary expand' onclick='itemHider(this);' style='margin:0;'>Remove</a>");
+  $("[id^=item-container]").find('[class^=table-stats]').append("<a id='button-add-storage'class='button secondary expand' onclick='addItem(this);' style='margin:0; width:50%'>Add to Storage</a>");
 });
 
 var script = document.createElement('script');
 script.appendChild(document.createTextNode('var showOffline='+showOffline+';'));
 script.appendChild(document.createTextNode('var offline='+JSON.stringify(offline)+';'));
+script.appendChild(document.createTextNode('var showStored='+showStored+';'));
+script.appendChild(document.createTextNode('var storedItems;'));
 script.appendChild(document.createTextNode(showItems));
 script.appendChild(document.createTextNode(itemHider));
 script.appendChild(document.createTextNode(showItems));
 script.appendChild(document.createTextNode(exportItems));
 script.appendChild(document.createTextNode(toggleOffline));
+script.appendChild(document.createTextNode(toggleStored));
+script.appendChild(document.createTextNode(addItem));
+script.appendChild(document.createTextNode(removeItem));
 (document.body || document.head || document.documentElement).appendChild(script);
 
-$("[id^=item-container]").find('[class^=first-cell]').append("<a class='button secondary expand' onclick='itemHider(this);' style='margin:0;'>Remove</a>");
-$("[class^=search-results-block]").prepend("<a class='button secondary expand' onclick='showItems()' style='width:33%'>Show all hidden items</a>");
-$("[class^=search-results-block]").prepend("<a class='button secondary expand' onclick='exportItems()' style='width:33%'>Export shown items</a>");
-$("[class^=search-results-block]").prepend("<a class='button secondary expand' onclick='toggleOffline()' style='width:33%'>Show / hide Offline</a>");
+$("[id^=item-container]").find('[class^=first-cell]').append("<a id='button-remove' class='button secondary expand' onclick='itemHider(this);' style='margin:0;'>Remove</a>");
+$("[id^=item-container]").find('[class^=table-stats]').append("<a id='button-add-storage'class='button secondary expand' onclick='addItem(this);' style='margin:0; width:50%'>Add to Storage</a>");
+$("[class^=search-results-block]").prepend("<div id='search-results-stored' style='display:none;'></div>");
+$("[id=search-results-stored]").prepend("<table class='search-results' id='search-results-stored-items'>");
+$("[class^=search-results-block]").prepend("<a class='button secondary expand' onclick='showItems()' style='width:25%'>Show all hidden items</a>");
+$("[class^=search-results-block]").prepend("<a class='button secondary expand' onclick='exportItems()' style='width:25%'>Export shown items</a>");
+$("[class^=search-results-block]").prepend("<a class='button secondary expand' onclick='toggleOffline()' style='width:25%'>Show / hide Offline</a>");
+$("[class^=search-results-block]").prepend("<a class='button secondary expand' onclick='toggleStored()' style='width:25%'>Stored Items</a>");
 $("[id^=mid-table").hide();
